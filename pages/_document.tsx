068ea527b9061df84b157b7ed87bf978c9870ffc
Besomi22 <sbelllali000@gmail.com>
2025-10-18 10:28:21 +0100

    update project structure and add head metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Analytics Dashboard</title>
+        <meta name="description" content="Overview of your dashboard" />
+        <link rel="icon" sizes="16x16" href="/icon2.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
